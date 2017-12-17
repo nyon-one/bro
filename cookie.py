02f5aed9f4295bb684ad54441ac1926edd6d4f54
Axter <one.nyon@gmail.com>
2017-12-17 15:07:50 +0545

    Add Chrome Profiles cookies support

--- cookie.py
+++ cookie.py
@@ -2,6 +2,15 @@ from http.cookiejar import LWPCookieJar
 from ..rel import Rel
 from . import Bro, SessionX
 
+from os import (path, environ)
+GOOGLE_USER_DATA = path.join(environ['LOCALAPPDATA'],
+	'Google', 'Chrome', 'User Data')
+
+def chrome_profile_join(*args):
+	o = glob(GOOGLE_USER_DATA+'\Profile *')
+	o.append(GOOGLE_USER_DATA+'\Default')
+	return (path.join(i, *args) for i in o)
+
 class LocalCookie(LWPCookieJar):
 	def __init__(self, file_name):
 		file_name = Rel(__file__).folder('.cook').join(file_name)
@@ -10,7 +19,12 @@ class LocalCookie(LWPCookieJar):
 	def isempty(self):
 		return len(self) == 0
 
-from browser_cookie3 import firefox
+from browser_cookie3 import (firefox, Chrome)
+
+def chrome_cook(profile='Default', filename='Cookies'):
+	for i in chrome_profile_join(filename):
+		o = Chrome(i)
+		if profile in i:return o.load()
 
 class Firefox(Bro):
 	USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0'
